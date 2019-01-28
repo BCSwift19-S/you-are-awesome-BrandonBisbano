401b6f80eec1f88fc5cb58cccd1bf4dbafd7310c
Brandon Bisbano <brandonbisbano@brandons-macbook-pro.local>
2019-01-27 23:03:47 -0500

    2.14 Complete

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -29,7 +29,8 @@ class ViewController: UIViewController {
                         "You are tremendous!",
                         "You've got the design skills of Jony Ive!"]
         
-        var newIndex = -1
+       // var newIndex = -1
+        var newIndex: Int // declares but doesn't initialize newIndex
         
         repeat {
             newIndex = Int.random(in: 0..<messages.count)
