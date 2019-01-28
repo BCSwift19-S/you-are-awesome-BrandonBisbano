d18c23f5cc120854d0dc7a2fdb2823680ab39ef8
Brandon Bisbano <brandonbisbano@brandons-macbook-pro.local>
2019-01-27 22:49:00 -0500

    2.13 Completed

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -29,13 +29,24 @@ class ViewController: UIViewController {
                         "You are tremendous!",
                         "You've got the design skills of Jony Ive!"]
         
+        var newIndex = -1
+        
+        repeat {
+            newIndex = Int.random(in: 0..<messages.count)
+        } while index == newIndex
+        
+        index = newIndex
         messageLabel.text = messages[index]
         
-        if  index == messages.count - 1 {
-            index = 0
-        } else {
-            index = index + 1
-        }
+//        messageLabel.text = messages.randomElement()!
+        
+//        messageLabel.text = messages[index]
+//
+//        if  index == messages.count - 1 {
+//            index = 0
+//        } else {
+//            index = index + 1
+//        }
         
 //        let message1 = "You Are Fantastic!!!"
 //        let message2 = "You Are Great!"
